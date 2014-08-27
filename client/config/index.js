267bdb3bead67bbd7fdf69aad5dfea06df71cdff
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-08-27 00:07:07 -0700

    remove all existing sortable stuff

--- client/config/index.js
+++ client/config/index.js
@@ -11,9 +11,8 @@ var DeactivateController = require('./controllers/deactivate');
 var HerokuController = require('./controllers/heroku');
 var GithubController = require('./controllers/github');
 var interpolate = require('./configs/interpolate');
-var ngSortableDirective = require('../utils/ng-sortable-directive');
 
-var app = angular.module('config', ['ui.bootstrap', 'ui.codemirror', 'ui.sortable', 'alerts', 'moment'])
+var app = angular.module('config', ['ui.bootstrap', 'ui.codemirror', 'alerts', 'moment'])
   .config(['$interpolateProvider', interpolate])
   .controller('Config', ['$scope', '$element', '$sce', ConfigController])
   .controller('RunnerController', ['$scope', '$element', RunnerController])
@@ -24,6 +23,5 @@ var app = angular.module('config', ['ui.bootstrap', 'ui.codemirror', 'ui.sortabl
   .controller('DeactivateCtrl', ['$scope', DeactivateController])
   .controller('HerokuController', ['$scope', HerokuController])
   .controller('GithubCtrl', ['$scope', GithubController])
-  .directive('ngSortable', ['$parse', ngSortableDirective]);
 
 module.exports = app;
